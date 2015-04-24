953d36b3d961d4fc2c57677842bf404997339719
dhagandev <dhagan@utexas.edu>
2015-04-24 14:25:38 -0700

    Added basic rotation

--- JollyJamGame/Assets/move.cs
+++ JollyJamGame/Assets/move.cs
@@ -11,9 +11,9 @@ public class move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-
 		transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime * 2,Input.GetAxis("Vertical")*Time.deltaTime * 2,0);
+		transform.Rotate(Vector3.right, Time.deltaTime);
+		//transform.Rotate(Vector3.up, Time.deltaTime, Space.World);
 	}
 	void OnTriggerStay(Collider c)
 	{
