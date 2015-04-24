e45226db1c68b11c0c07a8f64bc6d6034c289a0b
dhagandev <dhagan@utexas.edu>
2015-04-24 16:24:12 -0700

    Implemented border, rotation and movement; BUG: Center of rotation needs to be fixed on player

--- JollyJamGame/Assets/move.cs
+++ JollyJamGame/Assets/move.cs
@@ -1,22 +1,37 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class move : MonoBehaviour {
-
-
+	private float xAxis_rotate;
+	private float xAxis_move;
+	private float yAxis_move;
+	private Rigidbody2D rb2d;
+	public float alt_move;
+	public float goalSpeed;
 	// Use this for initialization
 	void Start () {
-	
+		rb2d = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime * 2,Input.GetAxis("Vertical")*Time.deltaTime * 2,0);
-		transform.Rotate(Vector3.right, Time.deltaTime);
-		//transform.Rotate(Vector3.up, Time.deltaTime, Space.World);
+		xAxis_move = Input.GetAxis ("HorizontalLeft");
+		yAxis_move = Input.GetAxis ("VerticalLeft");
+		xAxis_rotate = Input.GetAxis ("HorizontalRight");
 	}
-	void OnTriggerStay(Collider c)
+
+	void FixedUpdate () {
+		if (rb2d.velocity.sqrMagnitude < goalSpeed) {
+			rb2d.velocity = new Vector2(xAxis_move * alt_move, yAxis_move * alt_move * -1f);
+		}
+		rb2d.AddTorque (xAxis_rotate);
+	}
+
+	void OnCollisionEnter2D(Collision2D c)
 	{
-		c.transform.parent = this.transform;
+		print (c.gameObject);
+		if (c.gameObject.layer == LayerMask.NameToLayer("Pickups")) {
+			c.transform.parent = this.transform;
+		}
 	}
 }
