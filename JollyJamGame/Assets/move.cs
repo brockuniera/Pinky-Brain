5958e322d7478bad9b11b9cbaf4b20ac63312836
mjjohns <kaleidoscapester@gmail.com>
2015-04-25 12:22:55 -0500

    Made Unity project

---
+++ JollyJamGame/Assets/move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime * 2,Input.GetAxis("Vertical")*Time.deltaTime * 2,0);
+	}
+	void OnTriggerStay(Collider c)
+	{
+		c.transform.parent = this.transform;
+	}
+}
