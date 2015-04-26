805bd81684e48c9aad90668949591e44e7418a56
brockuniera <brockuniera@utexas.edu>
2015-04-25 22:07:54 -0500

    Add test assets, bullets kill metal

--- JollyJamGame/Assets/MetalPickup.cs
+++ JollyJamGame/Assets/MetalPickup.cs
@@ -2,8 +2,14 @@
 using System.Collections;
 
 public class MetalPickup : MonoBehaviour {
-
 	private bool _collected = false;
+
+	//Am I currently collectable by player?
+	private bool _collectable = true;
+	private Timer _collect_timer;
+	public float TimeToRecollect;
+
+	//My hit points
 	public int HitPoints = 1;
 
 	public void SetCollected(bool col){
@@ -12,12 +18,28 @@ public class MetalPickup : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		_collect_timer = new Timer();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(!_collectable && _collect_timer.isDone){
+			_collectable = true;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D c){
+		if(_collected){
+			//Getting hit by a bullet
+			if(c.gameObject.layer == LayerMask.NameToLayer("Bullets")){
+				print("BULLET HIT ME :(");
+				if(--HitPoints == 0){
+					Detach();
+					_collected = false;
+				}
+				Destroy(c.gameObject);
+			}
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D c)
@@ -27,13 +49,6 @@ public class MetalPickup : MonoBehaviour {
 			if(c.gameObject.layer == LayerMask.NameToLayer("Pickups")){
 				c.transform.parent = transform;
 			}
-			//Getting hit by a bullet
-			if(c.gameObject.layer == LayerMask.NameToLayer("Bullets")){
-				if(--HitPoints == 0){
-					Detach();
-					_collected = false;
-				}
-			}
 		}
 
 	}
@@ -41,6 +56,11 @@ public class MetalPickup : MonoBehaviour {
 	//Handles detaching this metal and child metals too
 	public void Detach(){
 		transform.parent = null;
+		Destroy(gameObject);
+		/*
+		_collectable = false;
+		_collect_timer.SetTimer(TimeToRecollect);
+		*/
 	}
 
 }
